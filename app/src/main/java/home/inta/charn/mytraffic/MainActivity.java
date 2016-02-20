c137c3afdd5617fc5b75eb74bb89f3707a7f1f05
charn <charnchalit@gmail.com>
2016-02-20 15:07:01 +0700

    Finish 20Feb16

--- app/src/main/java/home/inta/charn/mytraffic/MainActivity.java
+++ app/src/main/java/home/inta/charn/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package home.inta.charn.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -20,7 +24,30 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
     } // Main Method
+
+    private void buttonController() {
+
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.cat );
+                mediaPlayer.start();
+
+                //Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/0h3NeVv8AGg"));
+                startActivity(intent);
+
+            } //event
+        });
+
+    } //buttonController
 
     private void bindWidget() {
 
